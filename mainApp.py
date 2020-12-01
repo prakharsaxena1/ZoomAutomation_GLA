eaeccb78c2f39be1936a417676ed03db9599543b
Prakhar Saxena <prakharsaxena2806@gmail.com>
2020-12-01 22:02:49 +0530

    added more functions to mainApp.py

--- mainApp.py
+++ mainApp.py
@@ -1,6 +1,6 @@
 # chrome://settings/cookies/detail?site=glauniversity.in&search=cookies
 # C:\Users\prakh\AppData\Local\Google\Chrome\User Data\Default
-# Session ID:  ireedt31vdhs4f2qto13ggk0
+# Session ID:  pysl2rgq2odca1vk0d4z3yvo
 
 import time
 import datetime
@@ -8,20 +8,9 @@ import requests
 import json
 import subprocess
 from pynput.keyboard import Key, Controller
-import pyperclip
 import schedule
-
-
-def hitEnter(keyboard):
-    keyboard.press(Key.enter)
-    keyboard.release(Key.enter)
-
-
-def hitPaste(keyboard):
-    keyboard.press(Key.ctrl_l)
-    keyboard.press('v')
-    keyboard.release(Key.ctrl_l)
-    keyboard.release('v')
+import pyautogui
+import os
 
 
 def convert24Hours(number, x):
@@ -30,56 +19,87 @@ def convert24Hours(number, x):
         return number
     return int(number) + 12
 
-def checkTime():
-    a = str(datetime.datetime.now())
-    return a.split(" ")[1][0:2]
-    
+
+def tabenter():
+    keyboard = Controller()
+    keyboard.press(Key.tab)
+    keyboard.release(Key.tab)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+
+
 def SignIn(meetID, meetPassword):
     subprocess.call(["C:/Users/prakh/AppData/Roaming/Zoom/bin/Zoom.exe"])
     time.sleep(10)
-    keyboard = Controller()
-    pyperclip.copy(meetID)
-    for i in range(7):
-        keyboard.press(Key.tab)
-        keyboard.release(Key.tab)
-    time.sleep(5)
-    hitEnter(keyboard)
-    hitPaste(keyboard)
-    hitEnter(keyboard)
-    pyperclip.copy(meetPassword)
-    time.sleep(5)
-    hitPaste(keyboard)
-    for i in range(2):
-        keyboard.press(Key.tab)
-        keyboard.release(Key.tab)
-    hitEnter(keyboard)
+    # Big join button
+    join_btn = pyautogui.locateCenterOnScreen('join1.png')
+    pyautogui.moveTo(join_btn)
+    pyautogui.click()
+    # Meeting ID
+    time.sleep(2)
+    pyautogui.write(meetID)
+    # Disables: camera and mic
+    media_btn = pyautogui.locateAllOnScreen('cb.png')
+    for btn in media_btn:
+        pyautogui.moveTo(btn)
+        pyautogui.click()
+    tabenter()
+    pyautogui.moveTo(join_btn)
+    pyautogui.click()
+    time.sleep(2)
+    # Types the password and hits enter
+    pyautogui.write(meetPassword)
+    pyautogui.press('enter')
 
 
-sessionID = "ireedt31vdhs4f2qto13ggk0"
-# sessionID = input("Enter session ID:  ")
-date = "2020-12-1"
-# date = input("Enter date(FORMAT: YYYY-MM-D,no preceeding zeros):  ")
-GYOC = {"ASP.NET_SessionId": sessionID}
-data = {"text": date}
-url = "https://glauniversity.in:8085/MyAccount/DutyDetails"
-r = requests.post(url, data=data, cookies=GYOC)
-y = json.loads(r.text)
+def getTimeTable():
+    sessionID = "pysl2rgq2odca1vk0d4z3yvo"  # For testing purposes
+    # sessionID = input("Enter session ID:  ")
+    date = "2020-12-1"  # For testing purposes
+    # date = input("Enter date(FORMAT: YYYY-MM-D,no preceeding zeros):  ")
+    GYOC = {"ASP.NET_SessionId": sessionID}
+    data = {"text": date}
+    url = "https://glauniversity.in:8085/MyAccount/DutyDetails"
+    r = requests.post(url, data=data, cookies=GYOC)
+    y = json.loads(r.text)
+    X = []
+    for i in y:
+        number, x = i["TimeFrom"].split(" - ")[0].split(" ")
+        timed = convert24Hours(number, x)
+        meetingID, password = i["JoinUrl"][18::].split("?pwd=")
+        X.append([str(timed), meetingID, password])
+    X.sort()
+    timetable = open("timetable.txt", 'w')
+    for i in X:
+        timetable.write(str(i).replace("'", "").replace("]", "").replace("[", "")+"\n")
+    timetable.write("end, end, end")
+    timetable.close()
+    print("Timetable loaded")
 
-X = []
-for i in y:
-    number, x = i["TimeFrom"].split(" - ")[0].split(" ")
-    time = convert24Hours(number, x)
-    meetingID, password = i["JoinUrl"][18::].split("?pwd=")
-    X.append([str(time), meetingID, password])
-X.sort()
-timetable = open("timetable.txt", 'w')
-for i in X:
-    timetable.write(str(i).replace("'", "").replace(
-        "]", "").replace("[", "")+"\n")
-timetable.write("end, end, end")
-timetable.close()
 
-schedule.every().hour.do(job)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+def getIDPasswd(now):
+    meetID = ""
+    meetPasswd = ""
+    f = open("timetable.txt", 'r')
+    for i in f.readlines():
+        j = i.split(",")
+        if j[0] == "end":
+            f.close()
+            os.remove("timetable.txt")
+            print("Exiting App...")
+            exit()
+        elif int(j[0]) < int(now):
+            continue
+        else:
+            meetID, meetPasswd = j[1].strip(" "), j[2].strip("\n").strip(" ")
+            break
+    return (meetID, meetPasswd)
+
+if not os.path.exists("timetable.txt"):
+    getTimeTable()
+
+now = 12  # For testing purposes
+# now = datetime.datetime.now().strftime("%H")
+meetID, meetPasswd = getIDPasswd(now)
+print(meetID, meetPasswd)
+
