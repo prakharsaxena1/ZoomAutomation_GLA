64ae99b85f0d4d466f30e8c2a5979565589c202f
Prakhar Saxena <prakharsaxena2806@gmail.com>
2020-12-03 22:07:13 +0530

    reduced dependency on GLA portal

--- mainApp.py
+++ mainApp.py
@@ -7,6 +7,13 @@ import schedule
 import config
 import pyautogui
 import subprocess
+import os
+
+Previous_Date = datetime.datetime.today() - datetime.timedelta(days=1)
+date = datetime.date.today()
+if os.path.exists(f"datadump{Previous_Date}.json"):
+    os.remove(f"datadump{Previous_Date}.json")
+
 
 def convert24Hours(number, x):
     if x == "AM":
@@ -15,10 +22,9 @@ def convert24Hours(number, x):
 
 def SignIn(meetID, meetPassword):
     subprocess.call(["C:/Users/prakh/AppData/Roaming/Zoom/bin/Zoom.exe"])
-    time.sleep(10)
+    time.sleep(7)
     join_btn = pyautogui.locateCenterOnScreen('join1.png')
     pyautogui.moveTo(join_btn)
-    time.sleep(1)
     pyautogui.click()
     time.sleep(2)
     pyautogui.write(meetID)
@@ -27,25 +33,27 @@ def SignIn(meetID, meetPassword):
         pyautogui.moveTo(btn)
         pyautogui.click()
     pyautogui.press('tab')
-    time.sleep(0.5)
     pyautogui.press('enter')
     time.sleep(2)
     pyautogui.write(meetPassword)
     pyautogui.press('enter')
-    
+
 def getTimeTable():
-    sessionID = config.sessionID
-    date = datetime.date.today()
-    GYOC = {"ASP.NET_SessionId": sessionID}
-    data = {"text": date}
-    url = "https://glauniversity.in:8085/MyAccount/DutyDetails"
-    r = requests.post(url, data=data, cookies=GYOC)
-    try:
-        y = json.loads(r.text)
-    except json.decoder.JSONDecodeError as e:
-        print("SessionID has expired, will need a new one in order to continue")
-        exit()
-    x=[]
+    if not os.path.exists(f"datadump{date}.json"):
+        sessionID = config.sessionID
+        GYOC = {"ASP.NET_SessionId": sessionID}
+        data = {"text": date}
+        url = "https://glauniversity.in:8085/MyAccount/DutyDetails"
+        r = requests.post(url, data=data, cookies=GYOC)
+        try:
+            with open(f"datadump{date}.json", 'w') as f:
+                f.write(r.text)
+        except json.decoder.JSONDecodeError as e:
+            print("SessionID has expired, will need a new one in order to continue")
+            exit()
+    with open(f"datadump{date}.json") as f:
+        y = json.load(f)
+    x = []
     for i in y:
         a, b = i['TimeFrom'].split("-")[0].strip(" ").split(" ")
         time_ = convert24Hours(a[0:a.index(":")], b)
@@ -55,7 +63,7 @@ def getTimeTable():
     x.sort()
     for time_, link in x:
         meetID, meetPasswd = link[18::].split("?pwd=")
-        schedule.every().day.at(time_+":00").do(SignIn,meetID,meetPasswd)
+        schedule.every().day.at(time_+":00").do(SignIn, meetID, meetPasswd)
 
 getTimeTable()
 
